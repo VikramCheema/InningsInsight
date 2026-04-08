5ec0c93f0f472aeb3450f53a2c194d431550229c
Vikramdeep Singh <vikramdeepcheema@yahoo.in>
2026-04-08 11:30:07 +0100

    follow-up on last one

--- pages/05_Global_Ranking.py
+++ pages/05_Global_Ranking.py
@@ -171,7 +171,7 @@ with tab1:
                 display_text=r"^.*player=(.*)$" # This regex trick pulls the name back out of the URL for display
             ),
             "Team_Name":"Team",
-            "Matches_Played": "Matches",
+            "Matches_Played": "Matches", "Total_Runs":"Runs",
             "Bat_Avg": st.column_config.NumberColumn("Avg", format="%.2f"),
             "Bat_SR": st.column_config.NumberColumn("SR", format="%.2f"),
             "Pts_Batting": st.column_config.NumberColumn("Points", format="%.0f")
@@ -192,7 +192,7 @@ with tab2:
         df_bowl[cols_bowl],
         column_config={
             "Player_Link": st.column_config.LinkColumn("Player Name", display_text=r"^.*player=(.*)$"),
-            "Total Wickets": "Wickets", "Team_Name":"Team", "Matches_Played":"Matches",
+            "Total_Wickets": "Wickets", "Team_Name":"Team", "Matches_Played":"Matches",
             "Bowl_Avg": st.column_config.NumberColumn("Avg", format="%.2f"),
             "Bowl_Econ": st.column_config.NumberColumn("Econ", format="%.2f"),
             "Pts_Bowling": st.column_config.NumberColumn("Points", format="%.0f")
@@ -214,7 +214,7 @@ with tab3:
         column_config={
             "Player_Link": st.column_config.LinkColumn("Player Name", display_text=r"^.*player=(.*)$"),
             "Pts_AllRounder": st.column_config.NumberColumn("Points", format="%.0f"),
-            "Total Wickets": "Wickets", "Team_Name":"Team", "Matches_Played":"Matches", "Total_Runs":"Runs"
+            "Total_Wickets": "Wickets", "Team_Name":"Team", "Matches_Played":"Matches", "Total_Runs":"Runs"
         },
         use_container_width=True, hide_index=True
     )
